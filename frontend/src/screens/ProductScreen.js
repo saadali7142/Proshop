55267a64071c55bcf7b134315581ca94379e5fd4
saadali7142 <saadali7142@gmail.com>
2022-08-10 13:41:22 +0500

    Basic Static Frontend emplemented with Routes

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
+import Rating from "../components/Rating";
+import products from "../products";
+const ProductScreen = (props) => {
+  let params = useParams();
+
+  const product = products.find((p) => p._id === params.id);
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image fluid src={product.image} alt={product.name} />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <ListGroup>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Status: {product.name}</ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                className="d-grid m-0"
+                type="button"
+                disabled={product.countInStock === 0}
+              >
+                Add to Cart
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;
