e5ce04967bd6c071d024592e6c9af93e95de2d67
saadali7142 <saadali7142@gmail.com>
2022-08-11 12:21:36 +0500

    Mongodb setup completed with seeder script

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
+import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
 const app = express();
 
 dotenv.config();
@@ -8,15 +10,15 @@ app.get("/", (req, res) => {
   res.send("API IS RUNNING");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+connectDB();
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
+app.use((req, res, next) => {
+  console.log(req.originalUrl);
+  next();
 });
-
+app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
