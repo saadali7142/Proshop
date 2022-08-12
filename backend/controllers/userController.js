3f1c0316945b5b219ae361c172273ea4254484c5
saadali7142 <saadali7142@gmail.com>
2022-08-13 01:16:17 +0500

    backend user authentication completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import asyncHandler from "express-async-handler";
-
+import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
 // @desc Auth User & get token
@@ -16,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
@@ -24,4 +24,59 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// @desc Auth User & get token
+// @route POST /api/users/login
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data");
+  }
+});
+
+// @desc Get User Profile
+// @route GET /api/users/profile
+//@access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  console.log(
+    "🚀 ~ file: userController.js ~ line 32 ~ getUserProfile ~ user",
+    user
+  );
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("User not Found");
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
